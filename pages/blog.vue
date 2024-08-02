7392635b03173233eda4aae7e440f289ac6496a4
Andrew <andreytrinuma@gmail.com>
2024-08-02 14:10:18 +0700

    nested routes

---
+++ pages/blog.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const menu = [
+  {
+    title: 'Post 1',
+    path: '/blog/post-1',
+  },
+  {
+    title: 'Post 2',
+    path: '/blog/post-2',
+  },
+  {
+    title: 'Post 3',
+    path: '/blog/post-3',
+  },
+]
+
+const route = useRoute()
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in menu" :key="item.title">
+      <NuxtLink :to="item.path">{{ item.title }}</NuxtLink>
+    </li>
+  </ul>
+  <NuxtPage :page-key="route.path" />
+</template>
