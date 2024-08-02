d517d47fc14ee77d9d5f352baa57bb10be38985e
Andrew <andreytrinuma@gmail.com>
2024-08-02 12:52:15 +0700

    dynamic routes

---
+++ pages/shop/[slug].vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const route = useRoute()
+const product = reactive({
+  title: '',
+  contents: '',
+})
+
+let { title, contents } = toRefs(product)
+
+const products = [
+  {
+    slug: 'product-1',
+    title: 'Product-1',
+    contents: 'contents-1',
+  },
+  {
+    slug: 'product-2',
+    title: 'Product-2',
+    contents: 'contents-2',
+  },
+  {
+    slug: 'product-3',
+    title: 'Product-3',
+    contents: 'contents-3',
+  },
+]
+
+const found = products.find(item => item.slug === route.params.slug)
+if (found) {
+  product.title = found.title
+  product.contents = found.contents
+}
+</script>
+
+<template>
+  <h2>
+    {{ title }}
+  </h2>
+  <div>{{ contents }}</div>
+</template>
---
+++ pages/shop/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const menu = [
+  {
+    title: 'Product-1',
+    path: '/shop/product-1',
+  },
+  {
+    title: 'Product-2',
+    path: '/shop/product-2',
+  },
+  {
+    title: 'Product-3',
+    path: '/shop/product-3',
+  },
+]
+</script>
+
+<template>
+  <ul v-if="menu">
+    <li v-for="(item, index) in menu" :key="index">
+      <NuxtLink :to="item.path">{{ item.title }}</NuxtLink>
+    </li>
+  </ul>
+</template>
